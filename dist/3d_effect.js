c5eeb434f41c306922df49bf4209c9e539514fd7
Stephen Herd <stephen_h369@live.co.uk>
2020-10-31 17:12:36 +0000

    general css adjustments, 3d effect turned off for devices <1024px

--- dist/3d_effect.js
+++ dist/3d_effect.js
@@ -1,27 +1,40 @@
 const cards = document.querySelectorAll(".card");
+const media = window.matchMedia("(max-width: 1024px)");
 
 
 // Moving Event
 cards.forEach((card) => card.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 100;
-  let yAxis = (window.innerHeight / 2 - e.pageY) / -450;
-  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  if (media.matches) {
+    return;
+  } else {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 100;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / -450;
+    card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  }
 }));
 
 //Animate In
 cards.forEach((card) => card.addEventListener("mouseenter", (e) => {
-  card.style.transition = "none";
-  //Popout
-  let info = card.querySelector(".card-info");
-  info.style.transform = "translateZ(75px)"; info.style.transition = "all 0.75s ease-out";
+  if (media.matches) {
+    return;
+  } else {
+    card.style.transition = "none";
+    //Popout
+    let info = card.querySelector(".card-info");
+    info.style.transform = "translateZ(75px)"; info.style.transition = "all 0.75s ease-out";
+  }
 }));
 
 //Animate Out
 cards.forEach((card) => card.addEventListener("mouseleave", (e) => {
-  card.style.transition = "all 0.5s ease";
-  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
-  //Popback
-  let info = card.querySelector(".card-info");
-  info.style.transform = "translateZ(0px)";  info.style.transition = "all 0.5s ease-in";
+  if (media.matches) {
+    return;
+  } else {
+    card.style.transition = "all 0.5s ease";
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    //Popback
+    let info = card.querySelector(".card-info");
+    info.style.transform = "translateZ(0px)";  info.style.transition = "all 0.5s ease-in";
+  }
 }));
 
